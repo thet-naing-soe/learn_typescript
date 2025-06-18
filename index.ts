84d391aaca192620d01a72420f91973bd1e178d4
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-18 15:12:34 +0630

    type narrowing

--- index.ts
+++ index.ts
@@ -51,12 +51,26 @@ function completeOrder(orderId: number) {
   return order;
 }
 
+function getPizzaDetail(identifier: string | number) {
+  if (typeof identifier === "string") {
+    return menu.find(
+      (pizza) => pizza.name.toLowerCase() === identifier.toLowerCase()
+    );
+  } else {
+    return menu.find((pizza) => pizza.id === identifier);
+  }
+}
+
 addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 });
 addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 });
 addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 });
 
 placeOrder("Chicken Bacon Ranch");
+placeOrder("Pepperoni");
 completeOrder(1);
+placeOrder("Anchovy");
+placeOrder("Veggie");
+completeOrder(2);
 
 console.log("Menu:", menu);
 console.log("Cash in register:", cashInRegister);
